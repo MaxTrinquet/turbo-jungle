eb17ec742b05efd3583d7a2b114bd9e3a2ec5e32
MaxTrinquet <maxime.trinquet@gmail.com>
2023-05-09 22:50:41 +0200

    vercel

--- server/server.js
+++ server/server.js
@@ -26,3 +26,15 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 });
+
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'), function (err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    });
+  })
+}
